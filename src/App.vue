f0d9b3cd3a38742d00cd9faa23fb6a76e8fd02b3
selsmie <simonelsmie@hotmail.com>
2021-03-01 16:19:42 +0000

    promise all corrected

--- src/App.vue
+++ src/App.vue
@@ -55,20 +55,25 @@ export default {
     "location-chart": LocationChart
   },
   methods: {
-    getLaunchData: function() {
-      fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1600')
-        .then(res => res.json())
-        .then(data => this.launches = data.results)
-    },
-
     // getLaunchData: function() {
-    //   const fetch1 = fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1600')
-    //     .then(response => response.json())
-    //   const fetch2 = fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1700')
-    //     .then(response => response.json())
-    //   Promise.all([fetch1, fetch2])
-    //     .then(data => this.launches = (data.results.flat(1)))
+    //   fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1600')
+    //     .then(res => res.json())
+    //     .then(data => this.launches = data.results)
     // },
+
+    getLaunchData: function() {
+      const fetch1 = fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1600')
+        .then(response => response.json())
+      const fetch2 = fetch('https://lldev.thespacedevs.com/2.0.0/launch/?limit=100&offset=1700')
+        .then(response => response.json())
+      Promise.all([fetch1, fetch2])
+        .then(data => {
+          const updatedData = []
+          updatedData.push(data[0].results)
+          updatedData.push(data[1].results)
+          this.launches = updatedData.flat(1)
+        })
+    },
 
     dateYear: function(year) {
       this.filteredLaunches = []
